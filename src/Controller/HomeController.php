9a447d452298f180ee165c15dffda0e2164ce1ae
Julien Bonvarlet <julien.bonvarlet@faume.co>
2024-05-25 05:41:21 +0200

    wip
    Signed-off-by: Julien Bonvarlet <julien.bonvarlet@faume.co>

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
+    #[Route('/', name: 'home', methods: 'GET')]
     public function __invoke(): JsonResponse
     {
         return $this->json(['status' => 'OK']);
