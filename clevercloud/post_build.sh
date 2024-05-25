6fbc72d1086351d70f929e2b205a9fe417ea3f58
Julien Bonvarlet <julien.bonvarlet@faume.co>
2024-05-25 18:23:19 +0200

    wip
    Signed-off-by: Julien Bonvarlet <julien.bonvarlet@faume.co>

--- clevercloud/post_build.sh
+++ clevercloud/post_build.sh
@@ -8,7 +8,8 @@ php /usr/host/bin/composer-2.phar run-script --no-dev post-install-cmd; \
 php bin/console doctrine:migrations:migrate --no-interaction --all-or-nothing; \
 php bin/console cache:clear --env=prod --no-debug
 
-varnish_conf_file=${APP_HOME}/clevercloud/varnish.vcl
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+varnish_conf_file=${SCRIPT_DIR}/varnish.vcl
 
 envs=`printenv`
 for env in $envs
